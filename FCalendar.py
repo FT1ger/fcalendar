a8ff28853aec61ac0be8cd0f35b9695ebe5950d8
FT1ger <63837350+FT1ger@users.noreply.github.com>
2025-02-19 00:44:39 +1030

    The first version of FCalendar1.0

---
+++ FCalendar.py
@@ -0,0 +1,91 @@
+import datetime
+import email
+from icalendar import Calendar, Event,Timezone
+from email.parser import BytesParser
+from email import policy
+import pandas as pd
+import numpy as np
+filepath = 'Your Potential Timetable.eml'
+import re
+import uuid
+
+with open(filepath) as email_file:
+    email_message = email.message_from_file(email_file)
+content = (email_message.get_payload())
+
+content1 = content.replace('=\n','')
+new_p = re.compile('<td.*?>.*?</td>')
+lst1 = new_p.findall(content1)
+lst1 = lst1[:-1]
+
+clear_p = re.compile('</*?td.*?>')
+for i in range(len(lst1)):
+    lst1[i] = re.sub(clear_p,'',lst1[i])
+
+lst2 = np.array(lst1)
+lst2 = lst2.reshape((-1,9))
+# sort all the codes in pandas dataframe
+df = pd.DataFrame(lst2)
+df.columns = ['status','class','section','subject','course','seats open','duration&locations','dates','campus']
+time_loc = df["duration&locations"]
+for i in range(len(time_loc)):
+    time_loc[i] = time_loc[i].split('<br />')
+
+dura = df['dates']
+for i in range(len(dura)):
+    dura[i] = dura[i].split('<br />')
+# start time & end time -> datetime.datetime
+def duration(duradate:str):
+    duradate = duradate.split(' - ')
+    start = datetime.datetime.strptime(duradate[0], '%d/%m/%Y')
+    end = datetime.datetime.strptime(duradate[1], '%d/%m/%Y')
+    return(start,end)
+
+dura_date = []
+for i in range(len(dura)):
+    for j in range(len(dura[i])):
+        dura[i][j] = duration(dura[i][j])
+# dura[i][j][0] = starttime,dura[i][j][1] = endtime
+# i = course,j = time duration
+
+# seperate time & location
+p2 = r"^([MTWFS][a-z]*)\s([\d:amp\s]+)-([\d:amp\s]+)-\s(.+)$"
+match = re.match(p2,time_loc[0][0])
+
+weeks = []
+time_start = []
+time_end = []
+location = []
+for i in range(len(time_loc)):
+    weeks.append([])
+    time_start.append([])
+    time_end.append([])
+    location.append([])
+    for j in range(len(time_loc[i])):
+        match = re.match(p2,time_loc[i][j])
+        weeks[i].append(match.group(1))
+        time_start[i].append(match.group(2).strip())
+        time_end[i].append(match.group(3).strip())
+        location[i].append(match.group(4))
+
+# create event
+def create_event(cal,course:str,start_date,end_date,start_time,end_time,location,campus):
+    day = start_date
+    while day <= end_date:
+        event = Event()
+        event.add("DTSTART", datetime.datetime.strptime((str(day.date())+' '+start_time),'%Y-%m-%d %I:%M%p'))
+        event.add("DTEND", datetime.datetime.strptime((str(day.date())+' '+end_time),'%Y-%m-%d %I:%M%p'))
+        event.add("CREATED", datetime.datetime.now())
+        event.add("UID", uuid.uuid4())
+        event.add("SUMMARY", course)
+        event.add("DESCRIPTION", campus)
+        event.add("LOCATION", location)
+        day += datetime.timedelta(days = 7)
+        cal.add_component(event)
+
+with open('Your_timetable.ics','wb') as f:
+    cal = Calendar()
+    for i in range(len(df['subject'])):
+        for j in range(len(dura[i])):
+            create_event(cal,df['subject'][i]+' '+df['course'][i],dura[i][j][0],dura[i][j][1],time_start[i][j],time_end[i][j],location[i][j],df['campus'][i])
+    f.write(cal.to_ical())
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Calendar
+# FCalendar
+FCalendar is a small tool which can read your schedule from email and generate an iCalandar (.ics) file, which can be imported to your Calendar app.
+
+## Dependencies
+Make sure that you have installed those libraries:
+- pandas
+- numpy
+- icalendar
+
+## How can I use FCalendar
+
+1. Open your Timetable Planner.(https://access.adelaide.edu.au/sa/student/TimeTablePlanner.asp)
+2. Generate your Timetable.
+3. View it.
+4. Click the "Email" button (at the left-top of this page).
+5. Send it to your email.
+6. Download the email file and place it in the same directory of `FCalendar.py`.(Its name should be "`Your Potential Timetable.eml`")
+7. Run FCalendar.py
+8. Generate a file "`Your_timetable.ics`", send it to your phone or import it to your calendar app.
+
+(If you are using iOS, strongly advise you create a new calendar to manage all the events.)
+
+## Problem
+
+- Havn't be tested with android.
+- Potential errors inside.
+
+## Steps
+
+- [x] Import .eml file
+
+- [x] Read datas from email(.eml)
+
+- [x] Catch some specific words to get datas
+
+- [x] Put every data in right place with write form of .ics file
+
+- [x] Output the .ics file.
+
